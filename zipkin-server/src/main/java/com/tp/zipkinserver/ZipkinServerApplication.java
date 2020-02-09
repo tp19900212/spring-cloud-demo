9a3bb74c7d71f5395dde81d330f6065ce8dcd02c
田鹏 <javatp@163.com>
2020-02-09 20:01:13 +0800

    升级springboot和SpringCloud版本

--- zipkin-server/src/main/java/com/tp/zipkinserver/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/tp/zipkinserver/ZipkinServerApplication.java
@@ -1,17 +1,18 @@
-package com.tp.servicesleuth;
+package com.tp.zipkinserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+// import zipkin.server.internal.EnableZipkinServer;
 import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZipkinServer
-public class ServiceSleuthApplication {
+public class ZipkinServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceSleuthApplication.class, args);
+        SpringApplication.run(ZipkinServerApplication.class, args);
     }
 
 }
